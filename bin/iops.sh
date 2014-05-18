e359a95646e0d1f8d6c6cc324081411aadb96926
William E. T. III <w3ttr3y@w3ttr3y.com>
2014-05-17 23:48:23 -0400

    Code cleanup

--- bin/iops.sh
+++ bin/iops.sh
@@ -11,25 +11,60 @@ DEFAULT_SPLUNK_HOME="/opt/splunk"
 
 find_python() {
   #Try to find a variable indicating Splunk Home
-  if [[ -z "$SPLUNK_HOME" ]] ; then
-    if [[ -e "${SPLUNK_HOME}/bin/python" ]] ; then
+  if [ -z "$SPLUNK_HOME" ] ; then
+    if [ -e "${SPLUNK_HOME}/bin/python" ] ; then
       echo "${SPLUNK_HOME}/bin/python"
       return
     fi
   fi
 
   #Try to find Splunk Home -- since we should be running as an app in etc/apps/app_name/bin try ../../../../bin/python
-  if [[ -e "../../../../bin/python" ]] ; then
+  if [ -e "../../../../bin/python" ] ; then
     echo "../../../../bin/python"
     return
   fi
 
   #Try a default splunk home
-  if [[ -e "/opt/splunk/bin/python" ]] ; then
+  if [ -e "/opt/splunk/bin/python" ] ; then
     echo "/opt/splunk/bin/python"
+  fi
+
+  if [ -e "/Applications/splunk/bin/python" ] ; then
+    echo "/Applications/splunk/bin/python"
+  fi
+}
+
+needsudo() {
+  # check root permissions
+  if [ "$UID" != "0" ]; then
+    echo "sudo -n "
+    return
+  fi
+}
+
+# by nolan6000 
+# http://stackoverflow.com/questions/3915040/bash-fish-command-to-print-absolute-path-to-a-file/21188136#21188136
+get_abs_filename() {
+  # $1 : relative filename
+  if [ -d "$(dirname "$1")" ]; then
+    echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
   fi
 }
 
 
 PYTHON=$(find_python)
-sudo $PYTHON ./cc_iops.py "$@"
+SUDO=$(needsudo)
+OUTPUT=$($SUDO $PYTHON ./cc_iops.py "$@" 2>&1)
+
+#if [ "$OUTPUT" = "sudo: a password is required" ] ; then
+if [ "$OUTPUT" = "sudo: a password is required" ] || [ "$OUTPUT" = "sudo: sorry, a password is required to run sudo" ] ; then
+  me=$(get_abs_filename "$0")
+  CC=$(dirname $(get_abs_filename "$0"))
+  CC="${CC}/cc_iops.py"
+  echo "Unable to execute sudo as it required a password. Please edit the sudoers so $USER can run $me without a password."
+  echo "$USER    $HOSTNAME = NOPASSWD: $CC"
+  echo "Please be careful! If a non-root user owns or can write to $CC, they will effectively have passwordless root access to your system as they can modify the script"
+  exit 2
+fi
+
+echo $OUTPUT
